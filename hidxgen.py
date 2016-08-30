c02b18655cdbac1836858052d74abda584c5234b
Seulbae Kim <squizz617@gmail.com>
2016-08-30 18:05:22 +0900

    test module for parameterized granularity

--- hidxgen.py
+++ hidxgen.py
@@ -7,76 +7,204 @@ Created: August 22, 2016
 import sys
 import os
 import parseutility
+import time
 from hashlib import md5
 
-print "USAGE: python hidxgen.py REPONAME GRANULARITY (f for full-function)"
+# print "USAGE: python hidxgen.py REPONAME ABSLVL GRANULARITY (f for full-function)"
 
 """ GLOBALS """
 try:
 	os.mkdir("hidx")
 except OSError:
 	pass
-saveDir = os.path.join(os.getcwd(), "hidx")
-projDic = {}
-hashFileMap = {}
-absLvl = 0
+repoName = "ubuntu"
+# saveDir = os.path.join(os.getcwd(), "hidx")
+# projDic = {}
+# hashFileMap = {}
+# absLvl = 0
+projInstanceList = []
 
 """ OFFICIAL """
-if len(sys.argv) > 2:
+if len(sys.argv) == 2:
 	repoName = sys.argv[1]
-	granLevel = sys.argv[2]
 
-""" FOR TESTING """
-repoName = "ubuntu"
-granLevel = "4"
+print "Processing", repoName
+
+class project:
+	repoName = None
+	absLvl = None
+	granLvl = None
+	pid = None
+	timeCost = None
+	projDic = {}
+	hashFileMap = {}
+
+	def __init__(self, repoName, absLvl, granLvl, pid):
+		self.repoName = repoName
+		self.absLvl = absLvl
+		self.granLvl = granLvl
+		self.pid = pid
+		self.timeCost = 0
+		self.projDic = {}
+		self.hashFileMap = {}
+
+	def add_to_dictionary(self, funcLen, hashValue, path, funcId):
+		# print "adding", funcLen, hashValue, "to", self.pid
+		try:
+			self.projDic[funcLen].append(hashValue)
+		except KeyError:
+			self.projDic[funcLen] = [hashValue]
+
+		try:
+			self.hashFileMap[hashValue].extend([path, int(funcId)])
+		except KeyError:
+			self.hashFileMap[hashValue] = [path, int(funcId)]
+		# print self.pid, "has", len(self.projDic)
+
+	def write_to_file(self):
+		with open("hidx-timecost/hashmark_" + str(self.absLvl) + '_' + str(self.granLvl) + '_' + self.repoName + ".hidx", 'w') as fp:
+			fp.write(self.repoName + '\n')
+			for key in sorted(self.projDic):
+				fp.write(str(key) + '\t')
+				for h in list(set(self.projDic[key])):
+					fp.write(h + '\t')
+				fp.write('\n')
+				
+			fp.write('\n=====\n')
+
+			for key in sorted(self.hashFileMap):
+				fp.write(str(key) + '\t')
+				for f in self.hashFileMap[key]:
+					fp.write(str(f) + '\t')
+				fp.write('\n')
+		print "Wrote", "hidx/hashmark_" + str(self.absLvl) + '_' + str(self.granLvl) + '_' + self.repoName + ".hidx"
+
+pid = 0
+for granLvl in range(4, 11):
+	for absLvl in range(0, 5):
+		projInstance = project(repoName, absLvl, granLvl, pid)
+		projInstanceList.append(projInstance)
+		pid += 1
+for absLvl in range(0, 5):
+	projInstance = project(repoName, absLvl, "f", pid)
+	projInstanceList.append(projInstance)
+	pid += 1
+# print "REPO:", repoName, "ABS:", absLvl, "GRAN:", granLevel
+# for p in projInstanceList:
+# 	print p.pid, p.absLvl, p.granLvl
 
 vulList = []
 for vul in os.listdir("vul/" + repoName):
 	if vul.endswith("OLD.vul"):
 		vulList.append(vul)
 
-for vul in vulList:
+timeCostList = []
+for i in range(8):
+	timeCostList.append(0)
+
+ioCostList = []
+for i in range(8):
+	ioCostList.append(0)
+
+for vi, vul in enumerate(vulList):
+	print "\t" + str(vi+1) + "/" + str(len(vulList)), vul
 	fileName = os.path.join("vul", repoName, vul)
 	funcInstance = parseutility.parseFile(fileName)
 	for f in funcInstance:
-		print "Function:", f.name
-		print "Path:", f.parentFile
+		f.removeListDup()
 		path = f.parentFile
 		lineList = []
-		# windowList = []
-		if granLevel == "f":
-			print parseutility.normalize(parseutility.abstract(f, absLvl)[1])
-		else:
-			granLevel = int(granLevel)
-			body = parseutility.getBody(f.getOriginalFunction())
-			body = parseutility.removeComment(body)
-			body = body.replace("{", "").replace("}", "")
 
-			print body
-			for line in body.split('\n'):
-				if len(line) > 1:
-					lineList.append(line.strip())
+		body = parseutility.getBody(f.getOriginalFunction())
+		body = parseutility.removeComment(body)
 
-			for lidx in range(0, len(lineList)-granLevel+1):
-				# print lidx, lineList[lidx:lidx+granLevel]
-				window = parseutility.normalize(parseutility.abstractWindow(f, absLvl, lineList[lidx:lidx+granLevel])[1])
-				funcLen = len(window)
-				hashValue = md5(window).hexdigest()
+		for line in body.split('\n'):
+			if len(line) > 1:
+				lineList.append(line.strip())
 
-				try:
-					projDic[funcLen].append(hashValue)
-				except KeyError:
-					projDic[funcLen] = [hashValue]
+		vcnt = 0
+		for granLvl in range(4, 11):	# each granularity
+			begin = time.time()
+			print granLvl,
+			for lidx in range(0, len(lineList)-granLvl+1):	# each window
+				for i in range(vcnt, vcnt+5):	# i%5 becomes absLvl from 0 to 4
+					window = parseutility.normalize(parseutility.abstractWindow(f, i%5, lineList[lidx:lidx+granLvl])[1])
+					# print window
+					funcLen = len(window)
+					hashValue = md5(window).hexdigest()
+					# print funcLen, hashValue
+					projInstanceList[i].add_to_dictionary(funcLen, hashValue, path, f.funcId)
+					# print projInstance.pid
+					# projInstance
+					# print window
+					# print projInstance.pid, len(projInstance.projDic)
+					# print '\n'
+				# print '\n'
+			print "%.6f" % (time.time()-begin)
+			timeCostList[granLvl-4] += time.time()-begin
+			vcnt += 5
 
-				try:
-					hashFileMap[hashValue].extend([path, 1])
-				except KeyError:
-					hashFileMap[hashValue] = [path, 1]
+		granLvl = "f"
+		print granLvl,
+		begin = time.time()
+		for i in range(vcnt, vcnt+5):
+			window = parseutility.normalize(parseutility.abstract(f, i%5)[1])
+			funcLen = len(window)
+			hashValue = md5(window).hexdigest()
+			projInstanceList[i].add_to_dictionary(funcLen, hashValue, path, f.funcId)
+		print "%.6f" % (time.time()-begin)
+		timeCostList[7] += time.time()-begin
 
-		# print parseutility.getBody(f.getOriginalFunction())
-	for key in projDic:
-		print key, projDic[key]
-
-	for key in hashFileMap:
-		print key, hashFileMap[key]
+i = 0
+for p in projInstanceList:
+	print p.granLvl,
+	begin = time.time()
+	p.write_to_file()
+	ioCostList[i/5] += time.time()-begin
+	i += 1
 	# sys.exit()
+
+print "Time Cost"
+for t in timeCostList:
+	print t
+
+print "I/O Time Cost"
+for t in ioCostList:
+	print t
+
+
+
+		# if granLevel == "f":	# for function-level granularity
+		# 	body = parseutility.normalize(parseutility.abstract(f, absLvl)[1])
+		# 	funcLen = len(body)
+		# 	hashValue = md5(body).hexdigest()
+
+			
+
+		# else:	# for smaller granularity
+		# 	granLevel = int(granLevel)
+		# 	body = parseutility.getBody(f.getOriginalFunction())
+		# 	body = parseutility.removeComment(body)
+		# 	# body = body.replace("{", "").replace("}", "")
+
+		# 	for line in body.split('\n'):
+		# 		if len(line) > 1:
+		# 			lineList.append(line.strip())
+
+		# 	for lidx in range(0, len(lineList)-granLevel+1):
+		# 		window = parseutility.normalize(parseutility.abstractWindow(f, absLvl, lineList[lidx:lidx+granLevel])[1])
+		# 		funcLen = len(window)
+		# 		hashValue = md5(window).hexdigest()
+
+		# 		try:
+		# 			projDic[funcLen].append(hashValue)
+		# 		except KeyError:
+		# 			projDic[funcLen] = [hashValue]
+
+		# 		try:
+		# 			hashFileMap[hashValue].extend([path, 1])
+		# 		except KeyError:
+		# 			hashFileMap[hashValue] = [path, 1]
+
+
+
