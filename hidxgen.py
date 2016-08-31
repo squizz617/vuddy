0c69c8bc0dcdb6cf66d3e02f01c21a4d1d01ee43
Seulbae Kim <squizz617@gmail.com>
2016-08-31 18:07:21 +0900

    do abstraction and normalization only once.

--- hidxgen.py
+++ hidxgen.py
@@ -14,7 +14,7 @@ from hashlib import md5
 
 """ GLOBALS """
 try:
-	os.mkdir("hidx")
+	os.mkdir("hidx-retested")
 except OSError:
 	pass
 repoName = "ubuntu"
@@ -91,8 +91,8 @@ for absLvl in range(0, 5):
 	pid += 1
 # print "REPO:", repoName, "ABS:", absLvl, "GRAN:", granLevel
 # for p in projInstanceList:
-# 	print p.pid, p.absLvl, p.granLvl
-
+	# print p.pid, p.absLvl, p.granLvl
+# sys.exit()
 vulList = []
 for vul in os.listdir("vul/" + repoName):
 	if vul.endswith("OLD.vul"):
@@ -115,26 +115,48 @@ for vi, vul in enumerate(vulList):
 		path = f.parentFile
 		lineList = []
 
-		body = parseutility.getBody(f.getOriginalFunction())
-		body = parseutility.removeComment(body)
+		abstractBodyList = []
+		for absLvl in range(5):
+			abstractBodyList.append(parseutility.removeComment(parseutility.abstract(f, absLvl)[1]))
 
-		for line in body.split('\n'):
-			if len(line) > 1:
-				lineList.append(line.strip())
+		abstractNormalizedBodyLinesList = []	# list of linelist
+		for abstractBody in abstractBodyList:
+			lineList = []
+			for line in abstractBody.split('\n'):
+				normLine = parseutility.normalize(line)
+				if len(normLine) > 1:
+					lineList.append(normLine)
+			abstractNormalizedBodyLinesList.append(lineList)
+
+		# for a in abstractNormalizedBodyLinesList:
+		# 	print a
+		# sys.exit()
 
 		vcnt = 0
 		for granLvl in range(4, 11):	# each granularity
 			begin = time.time()
 			print granLvl,
-			for lidx in range(0, len(lineList)-granLvl+1):	# each window
-				for i in range(vcnt, vcnt+5):	# i%5 becomes absLvl from 0 to 4
-					window = parseutility.normalize(parseutility.abstractWindow(f, i%5, lineList[lidx:lidx+granLvl])[1])
+			for i in range(vcnt, vcnt+5):	# i%5 becomes absLvl from 0 to 4
+				absLvl = i%5
+				# print "ABST:", absLvl
+				lineList = abstractNormalizedBodyLinesList[absLvl-5]
+				if len(lineList)-granLvl+1 < 0:
+					# print "ABORT"
+					continue
+				# else:
+				# 	print "RANGE:", "0 ~", len(lineList)-granLvl+1
+
+				for lidx in range(0, len(lineList)-granLvl+1):	# each window
+					window = lineList[lidx:lidx+granLvl]
+					window = ''.join(window)
+					# print window
+					# window = parseutility.normalize(parseutility.abstractWindow(f, i%5, lineList[lidx:lidx+granLvl])[1])
 					# print window
 					funcLen = len(window)
 					hashValue = md5(window).hexdigest()
 					# print funcLen, hashValue
-					projInstanceList[i].add_to_dictionary(funcLen, hashValue, path, f.funcId)
-					# print projInstance.pid
+					projInstanceList[(granLvl-4)*5+absLvl].add_to_dictionary(funcLen, hashValue, path, f.funcId)
+					# print "PID:", projInstanceList[(granLvl-4)*5+absLvl].pid
 					# projInstance
 					# print window
 					# print projInstance.pid, len(projInstance.projDic)
@@ -148,29 +170,33 @@ for vi, vul in enumerate(vulList):
 		print granLvl,
 		begin = time.time()
 		for i in range(vcnt, vcnt+5):
-			window = parseutility.normalize(parseutility.abstract(f, i%5)[1])
+			# print i
+			absLvl = i%5
+			lineList = abstractNormalizedBodyLinesList[absLvl-5]
+			window = ''.join(lineList)
+			# window = parseutility.normalize(parseutility.abstract(f, i%5)[1])
 			funcLen = len(window)
 			hashValue = md5(window).hexdigest()
 			projInstanceList[i].add_to_dictionary(funcLen, hashValue, path, f.funcId)
 		print "%.6f" % (time.time()-begin)
 		timeCostList[7] += time.time()-begin
 
-i = 0
-for p in projInstanceList:
-	print p.granLvl,
-	begin = time.time()
-	p.write_to_file()
-	ioCostList[i/5] += time.time()-begin
-	i += 1
-	# sys.exit()
+# i = 0
+# for p in projInstanceList:
+# 	print p.granLvl,
+# 	begin = time.time()
+# 	p.write_to_file()
+# 	ioCostList[i/5] += time.time()-begin
+# 	i += 1
+# 	# sys.exit()
 
-print "Time Cost"
-for t in timeCostList:
-	print t
+# print "Time Cost"
+# for t in timeCostList:
+# 	print t
 
-print "I/O Time Cost"
-for t in ioCostList:
-	print t
+# print "I/O Time Cost"
+# for t in ioCostList:
+# 	print t
 
 
 
