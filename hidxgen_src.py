d423fe86d3ccf075f58b52768fa225654e859acc
Seulbae Kim <squizz617@gmail.com>
2016-09-08 16:28:56 +0900

    assuming granlvl to be 'f'

--- hidxgen_src.py
+++ hidxgen_src.py
@@ -19,10 +19,10 @@ print "loading source",
 srcFileList = parseutility.loadSource(targetDir)
 print "(done)"
 
-locList = os.listdir("vul-per-length")
-for li, loc in enumerate(locList):
-	locList[li] = int(loc)
-locList = sorted(locList)
+# locList = os.listdir("vul-per-length")
+# for li, loc in enumerate(locList):
+# 	locList[li] = int(loc)
+# locList = sorted(locList)
 
 time0 = time.time()
 
@@ -30,100 +30,103 @@ numFiles = len(srcFileList)
 numFuncs = 0
 numLines = 0
 
-if intendedGranLvl == 'f':
-	for si, srcFile in enumerate(srcFileList):
-		print si+1, '/', len(srcFileList), srcFile
-		functionInstanceList = parseutility.parseFile(srcFile)
+# if intendedGranLvl == 'f':
+for si, srcFile in enumerate(srcFileList):
+	print si+1, '/', len(srcFileList), srcFile
+	functionInstanceList = parseutility.parseFile(srcFile)
 
-		numFuncs += len(functionInstanceList)
+	numFuncs += len(functionInstanceList)
 
-		for f in functionInstanceList:
-			f.removeListDup()
-			numLines += f.parentNumLoc
-			path = f.parentFile
-			absBody = parseutility.abstract(f, intendedAbsLvl)[1]
-			absBody = parseutility.normalize(absBody)
-			funcLen = len(absBody)
-			hashValue = hashlib.md5(absBody).hexdigest()
+	for f in functionInstanceList:
+		f.removeListDup()
+		numLines += f.parentNumLoc
+		path = f.parentFile
+		absBody = parseutility.abstract(f, intendedAbsLvl)[1]
+		absBody = parseutility.normalize(absBody)
+		funcLen = len(absBody)
+		hashValue = hashlib.md5(absBody).hexdigest()
 
-			try:
-				projDictList[intendedAbsLvl][funcLen].append(hashValue)
-			except KeyError:
-				projDictList[intendedAbsLvl][funcLen] = [hashValue]
+		try:
+			projDictList[intendedAbsLvl][funcLen].append(hashValue)
+		except KeyError:
+			projDictList[intendedAbsLvl][funcLen] = [hashValue]
 
-			try:
-				hashFileMapList[intendedAbsLvl][hashValue].extend([f.parentFile, f.funcId])
-			except KeyError:
-				hashFileMapList[intendedAbsLvl][hashValue] = [f.parentFile, f.funcId]
+		try:
+			hashFileMapList[intendedAbsLvl][hashValue].extend([f.parentFile, f.funcId])
+		except KeyError:
+			hashFileMapList[intendedAbsLvl][hashValue] = [f.parentFile, f.funcId]
 
-	for i in range(0, 5):
-		if i == intendedAbsLvl:
-			with open("hidx-target/hashmark_" + str(i) + '_' + str(intendedGranLvl) + '_' + projName + ".hidx", 'w') as fp:
-				fp.write(projName + '\n')
-				for key in sorted(projDictList[i]):
-					fp.write(str(key) + '\t')
-					for h in list(set(projDictList[i][key])):
-						fp.write(h + '\t')
-					fp.write('\n')
-				
-				fp.write('\n=====\n')
+# for i in range(0, 5):
+# 	if i == intendedAbsLvl:
+packageInfo = str(projName) + ' ' + str(numFiles) + ' ' + str(numFuncs) + ' ' + str(numLines) + '\n'
+with open("hidx-target/hashmark_" + str(intendedAbsLvl) + '_' + str(intendedGranLvl) + '_' + projName + ".hidx", 'w') as fp:
+	fp.write(packageInfo)
+	for key in sorted(projDictList[intendedAbsLvl]):
+		fp.write(str(key) + '\t')
+		for h in list(set(projDictList[intendedAbsLvl][key])):
+			fp.write(h + '\t')
+		fp.write('\n')
+	
+	fp.write('\n=====\n')
 
-				for key in sorted(hashFileMapList[i]):
-					fp.write(str(key) + '\t')
-					for f in hashFileMapList[i][key]:
-						fp.write(str(f) + '\t')
-					fp.write('\n')
+	for key in sorted(hashFileMapList[intendedAbsLvl]):
+		fp.write(str(key) + '\t')
+		for f in hashFileMapList[intendedAbsLvl][key]:
+			fp.write(str(f) + '\t')
+		fp.write('\n')
 
-else:
-	for granLvl in locList:
-		if granLvl != intendedGranLvl:
-			continue
-		for si, srcFile in enumerate(srcFileList):
-			print si+1, '/', len(srcFileList), srcFile
-			functionInstanceList = parseutility.parseFile(srcFile)
+# else:
+# 	for granLvl in locList:
+# 		if granLvl != intendedGranLvl:
+# 			continue
+# 		for si, srcFile in enumerate(srcFileList):
+# 			print si+1, '/', len(srcFileList), srcFile
+# 			functionInstanceList = parseutility.parseFile(srcFile)
 
-			for f in functionInstanceList:
-				f.removeListDup()
-				path = f.parentFile
-				absBody = parseutility.abstract(f, intendedAbsLvl)[1]
-				lineList = []
-				for line in absBody.split('\n'):
-					normLine = parseutility.normalize(line)
-					if len(normLine) > 1:
-						lineList.append(normLine)
+# 			for f in functionInstanceList:
+# 				f.removeListDup()
+# 				path = f.parentFile
+# 				absBody = parseutility.abstract(f, intendedAbsLvl)[1]
+# 				lineList = []
+# 				for line in absBody.split('\n'):
+# 					normLine = parseutility.normalize(line)
+# 					if len(normLine) > 1:
+# 						lineList.append(normLine)
 
-				for lidx in range(0, len(lineList)-granLvl+1):
-					window = ''.join(lineList[lidx:lidx+granLvl])
-					funcLen = len(window)
-					hashValue = hashlib.md5(window).hexdigest()
+# 				for lidx in range(0, len(lineList)-granLvl+1):
+# 					window = ''.join(lineList[lidx:lidx+granLvl])
+# 					funcLen = len(window)
+# 					hashValue = hashlib.md5(window).hexdigest()
 
-					try:
-						projDictList[intendedAbsLvl][funcLen].append(hashValue)
-					except KeyError:
-						projDictList[intendedAbsLvl][funcLen] = [hashValue]
+# 					try:
+# 						projDictList[intendedAbsLvl][funcLen].append(hashValue)
+# 					except KeyError:
+# 						projDictList[intendedAbsLvl][funcLen] = [hashValue]
 
-					try:
-						hashFileMapList[intendedAbsLvl][hashValue].extend([f.parentFile, f.funcId])
-					except KeyError:
-						hashFileMapList[intendedAbsLvl][hashValue] = [f.parentFile, f.funcId]
+# 					try:
+# 						hashFileMapList[intendedAbsLvl][hashValue].extend([f.parentFile, f.funcId])
+# 					except KeyError:
+# 						hashFileMapList[intendedAbsLvl][hashValue] = [f.parentFile, f.funcId]
 
-		for i in range(0, 5):
-			if i == intendedAbsLvl:
-				with open("hidx-target/hashmark_" + str(i) + '_' + str(granLvl) + '_' + projName + ".hidx", 'w') as fp:
-					fp.write(projName + '\n')
-					for key in sorted(projDictList[i]):
-						fp.write(str(key) + '\t')
-						for h in list(set(projDictList[i][key])):
-							fp.write(h + '\t')
-						fp.write('\n')
+# 		for i in range(0, 5):
+# 			if i == intendedAbsLvl:
+# 				packageInfo = str(projName) + ' ' + str(numFiles) + ' ' + str(numFuncs) + ' ' + str(numLines) + '\n'
+
+# 				with open("hidx-target/hashmark_" + str(i) + '_' + str(granLvl) + '_' + projName + ".hidx", 'w') as fp:
+# 					fp.write(packageInfo)
+# 					for key in sorted(projDictList[i]):
+# 						fp.write(str(key) + '\t')
+# 						for h in list(set(projDictList[i][key])):
+# 							fp.write(h + '\t')
+# 						fp.write('\n')
 					
-					fp.write('\n=====\n')
+# 					fp.write('\n=====\n')
 
-					for key in sorted(hashFileMapList[i]):
-						fp.write(str(key) + '\t')
-						for f in hashFileMapList[i][key]:
-							fp.write(str(f) + '\t')
-						fp.write('\n')
+# 					for key in sorted(hashFileMapList[i]):
+# 						fp.write(str(key) + '\t')
+# 						for f in hashFileMapList[i][key]:
+# 							fp.write(str(f) + '\t')
+# 						fp.write('\n')
 
 time1 = time.time()
 print "Elapsed time:", time1 - time0
