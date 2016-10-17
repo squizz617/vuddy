b5a3591151edba7e1f16b03772e072ab13d73b74
Seulbae Kim <squizz617@gmail.com>
2016-10-18 00:05:47 +0900

    codesensor2 -> FuncParser renamed

---
+++ FuncParser/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SOURCE_DIR='src'
+BUILD_DIR='build'
+ANTLR_JAR='antlr-4.5.3-complete.jar'
+RESULT_JAR='FuncParser.jar'
+
+rm -rf ${RESULT_JAR}
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+
+# Copy source files to build dir
+
+cp ./${SOURCE_DIR}/*.g4 ${BUILD_DIR}
+cp ./${SOURCE_DIR}/*.java ${BUILD_DIR}
+cp ./manifest.mf ${BUILD_DIR}
+cp $ANTLR_JAR ${BUILD_DIR}
+
+cd ${BUILD_DIR}
+
+# Generate Lexer and Parser from Grammar
+
+#java -jar ../${ANTLR_JAR} CPPGrammar.g
+java -cp ${ANTLR_JAR} org.antlr.v4.Tool Module.g4 Function.g4
+
+# Compile java-files
+javac -cp ./${ANTLR_JAR} ./*.java -Xlint:unchecked
+
+# unpack ANTLR-jar since we need some of the class files
+
+jar xf ./${ANTLR_JAR}
+
+# Create CodeSensor.jar
+jar cvfm ${RESULT_JAR} manifest.mf *.class org > /dev/null
+cp ${RESULT_JAR} ../
