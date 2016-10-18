36ba3e7985a9440f7946a0fdbd6b5286a2b20dd7
Seulbae Kim <squizz617@gmail.com>
2016-10-18 22:59:03 +0900

    fixed indent error for osx

--- hmark/spec_generator.py
+++ hmark/spec_generator.py
@@ -56,34 +56,34 @@ exe = EXE(pyz,\n\
 
 elif osName == "OSX":
 		fp.write("\
-	# -*- mode: python -*-\n\n\
-	block_cipher = None\n\n\n\
-	a = Analysis(['hmark.py'],\n\
-	             pathex=['" + cwd + "'],\n\
-	             binaries=None,\n\
-	             datas=None,\n\
-	             hiddenimports=[],\n\
-	             hookspath=[],\n\
-	             runtime_hooks=[],\n\
-	             excludes=[],\n\
-	             win_no_prefer_redirects=False,\n\
-	             win_private_assemblies=False,\n\
-	             cipher=block_cipher)\n\
-	a.datas += [('icon.gif', '" + os.path.join(cwd, 'icon.gif') + "', 'DATA')]\n\
-	a.datas += [('FuncParser.jar', '" + os.path.join(cwd, 'FuncParser.jar') + "', 'DATA')]\n\
-	pyz = PYZ(a.pure, a.zipped_data,\n\
-	             cipher=block_cipher)\n\
-	exe = EXE(pyz,\n\
-	          a.scripts,\n\
-	          a.binaries,\n\
-	          a.zipfiles,\n\
-	          a.datas,\n\
-	          name='HMark_" + version + "_" + osName + "',\n\
-	          debug=False,\n\
-	          strip=False,\n\
-	          upx=True,\n\
-	          console=True )\n\
-	""")
+# -*- mode: python -*-\n\n\
+block_cipher = None\n\n\n\
+a = Analysis(['hmark.py'],\n\
+             pathex=['" + cwd + "'],\n\
+             binaries=None,\n\
+             datas=None,\n\
+             hiddenimports=[],\n\
+             hookspath=[],\n\
+             runtime_hooks=[],\n\
+             excludes=[],\n\
+             win_no_prefer_redirects=False,\n\
+             win_private_assemblies=False,\n\
+             cipher=block_cipher)\n\
+a.datas += [('icon.gif', '" + os.path.join(cwd, 'icon.gif') + "', 'DATA')]\n\
+a.datas += [('FuncParser.jar', '" + os.path.join(cwd, 'FuncParser.jar') + "', 'DATA')]\n\
+pyz = PYZ(a.pure, a.zipped_data,\n\
+             cipher=block_cipher)\n\
+exe = EXE(pyz,\n\
+          a.scripts,\n\
+          a.binaries,\n\
+          a.zipfiles,\n\
+          a.datas,\n\
+          name='HMark_" + version + "_" + osName + "',\n\
+          debug=False,\n\
+          strip=False,\n\
+          upx=True,\n\
+          console=True )\n\
+""")
 
 elif osName == "Win":
 		fp.write("\
