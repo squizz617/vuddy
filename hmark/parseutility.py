aac81edb5f762ec923ca343067f75900d2dc3ed4
test <dngthe93@gmail.com>
2016-10-22 12:09:26 -0700

    Fix HMark path-related error (issue #6)
     - Add double quotes in javaCallCommand
       (hmark/parseutility.py)

--- hmark/parseutility.py
+++ hmark/parseutility.py
@@ -49,13 +49,13 @@ def setEnvironment(caller):
 		elif osName == 'l' or osName == "osx":
 			# full_path = os.path.join(base_path, "FuncParser.jar")
 			# javaCallCommand = "java -Xmx1024m -jar " + full_path + " "
-			javaCallCommand = "java -Xmx1024m -jar " + os.path.join(cwd, "FuncParser.jar") + " "
+			javaCallCommand = "java -Xmx1024m -jar \"" + os.path.join(cwd, "FuncParser.jar") + "\" "
 
 	else:
 		if osName == 'w':
 			javaCallCommand = "FuncParser.exe "
 		elif osName == 'l' or osName == "osx":
-			javaCallCommand = "java -Xmx1024m -jar FuncParser.jar "
+			javaCallCommand = "java -Xmx1024m -jar \"FuncParser.jar\" "
 
 
 class function:
