a8538b61ebe1d54cf571415737ccf56c5bd36cfc
Hajin <jb6804@naver.com>
2017-05-15 15:27:16 +0900

    Config boilerplate

--- config.py
+++ config.py
@@ -1,4 +1,28 @@
-#gitStoragePath = "/media/squizz/VM-mount/data/gitrepos/"
-gitStoragePath = "D:\\Jang\\Build\\CCSLab\\gitrepos"
-gitBinary = 'C:\\Program Files\\Git\\bin\\git.exe'
-javaBinary = 'java'
+import platform
+
+"""
+[Note for Windows]
+- Use '\\' or '/' in path
+Ex) gitStoragePath = "D:\\Source\\gitrepos"
+- Install 'Git for Windows'
+- Windows version of VUDDY use its own JRE
+
+[Note for POSIX]
+- Use '/' for path
+Ex) gitStoragePath = "/home/ubuntu/gitrepos/"
+- Java binary is only needed in Linux
+"""
+
+gitStoragePath = "/home/ubuntu/gitrepos/"
+gitBinary = "git"
+diffBinary = "diff"
+javaBinary = "java"
+
+pf = platform.platform()
+if "Windows" in pf:
+    gitBinary = "C:\\Program Files\\Git\\bin\\git.exe"
+    diffBinary = "C:\\Program Files\\Git\\usr\\bin\\diff.exe"
+elif "Linux" in pf:
+    gitBinary = "git"
+    diffBinary = "diff"
+    javaBinary = "java"
