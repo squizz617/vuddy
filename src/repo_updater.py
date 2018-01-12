e34c91232c5842bcade47a25b37148ee7c597100
Seulbae Kim <squizz617@gmail.com>
2018-01-12 16:56:39 +0900

    with logging function (to run it at the background)

--- src/repo_updater.py
+++ src/repo_updater.py
@@ -5,11 +5,17 @@ import urllib2
 import json
 import logging
 
-logging.basicConfig(filename='repo_updater.log', level=logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler("repo_updater.log")
+file_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('REPO',
-                    help='''Repository name''')
+parser.add_argument('REPO', help='''Repository name''')
 
 args = parser.parse_args()
 
@@ -39,14 +45,16 @@ if not os.path.isdir(repo_base):
 
 for ri, repo in enumerate(repo_list):
     target_dir = os.path.join(repo_base, repo)
-    print ri+1, "/", len(repo_list), "\t", target_dir,
+    infostr = str(ri+1) + "/" + str(len(repo_list)) + "\t" + repo
     if os.path.isdir(target_dir):
-        print "exists. PULL"
+        infostr += " EXISTS (PULL)"
+        logger.info(infostr)
         os.chdir(target_dir)
         os.system("git pull")
         os.chdir(cwd)
     else:
-        print "NULL. CLONE."
+        infostr += " DOESN'T EXIST (CLONE)"
+        logger.info(infostr)
         os.system("git clone {0}{1} {2}".format(url_base, repo, target_dir))
     
 
